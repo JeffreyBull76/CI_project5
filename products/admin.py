d05180e504efcbde763ddfc6cbbff97ad979e724
JeffreyBull76 <jeffreybull@sky.com>
2023-06-02 00:55:23 +0000

    custom admin and base products page

--- products/admin.py
+++ products/admin.py
@@ -2,6 +2,42 @@ from django.contrib import admin
 from .models import Product, Category, Review
 
 # Register your models here.
-admin.site.register(Product)
-admin.site.register(Category)
-admin.site.register(Review)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'image',
+        'display_reviews',  # Add custom method here
+    )
+    ordering = ('sku',)
+
+    def display_reviews(self, obj):
+        reviews = obj.reviews.all()
+        return ", ".join([f"{review.user.username} ({review.rating})" for review in reviews])  # noqa
+
+    display_reviews.short_description = 'Reviews'
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = (
+        'user',
+        'product',
+        'rating',
+    )
+
+
+# Register your models here.
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Review, ReviewAdmin)
