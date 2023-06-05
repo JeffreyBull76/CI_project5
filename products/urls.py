f16d7289a4e9fe7232879a7e1557ed2aa67b46f8
JeffreyBull76 <jeffreybull@sky.com>
2023-06-05 00:45:56 +0000

    style and add product detail page

--- products/urls.py
+++ products/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('categories/<int:category_id>/products/', views.category_products, name='category_products'),  # noqa
+    path('<product_id>', views.product_detail, name='product_detail'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,13 +1,26 @@
-from django.shortcuts import render
-from .models import Product
+from django.shortcuts import render, get_object_or_404
+from .models import Product, Category
 
 
 def category_products(request, category_id):
-    # Logic to retrieve products for the given category_id
+    # Logic to retrieve the category and products
+    category = Category.objects.get(id=category_id)
     products = Product.objects.filter(category_id=category_id)
 
     context = {
+        'category_name': category.get_friendly_name(),
         'products': products
     }
 
     return render(request, 'products/products.html', context)
+
+
+def product_detail(request, product_id):
+    # Logic to retrieve product details
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product
+    }
+
+    return render(request, 'products/product_detail.html', context)
