95e9a82de8411d17b360663be35ba2277ab6a897
JeffreyBull76 <jeffreybull@sky.com>
2023-06-12 21:32:02 +0000

    add comment form and management

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
+from .views import contact_form_management_view
 
 urlpatterns = [
     path('', views.index, name='home'),
     path('contact/', views.contact_form_view, name='contact'),
+    path('contact-management/', contact_form_management_view, name='contact_management'),  # noqa
+    path('contact-management/delete/<int:message_id>/', views.delete_message, name='delete_message'),  # noqa
 ]
--- home/views.py
+++ home/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404, reverse  # noqa
 from products.models import Category, Product
 from .forms import ContactFormModelForm
+from .models import ContactForm
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 from django.shortcuts import render, redirect
-from .forms import ContactFormModelForm
 from django.core.mail import send_mail, BadHeaderError
 from django.http import HttpResponse
 
@@ -49,8 +51,35 @@ def contact_form_view(request):
             form.save()
             # Send email with form data
             send_contact_email(form.cleaned_data)
+            messages.success(request, 'Successfully sent message!')
             # Redirect to the success page
             return redirect('home')
     else:
         form = ContactFormModelForm()
     return render(request, 'home/contact_form.html', {'form': form})
+
+
+@login_required
+def contact_form_management_view(request):
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
+    contact_forms = ContactForm.objects.all()
+    return render(request, 'home/contact_form_management.html', {'contact_forms': contact_forms})  # noqa
+
+
+@login_required
+def delete_message(request, message_id):
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
+    message = get_object_or_404(ContactForm, form_id=message_id)
+
+    if request.method == 'POST':
+        message.delete()
+
+    messages.success(request, 'Message deleted!')
+    # Redirect to contact management page after deletion
+    return redirect('contact_management')
